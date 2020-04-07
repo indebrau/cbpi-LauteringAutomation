704897ab604924eda4db3e653026d29d2ec2fbb1
pedela <lange@dbis.rwth-aachen.de>
2020-04-07 22:09:41 +0200

    init

---
+++ __init__.py
@@ -0,0 +1,34 @@
+from modules import cbpi
+from modules.core.controller import KettleController
+from modules.core.props import Property
+
+@cbpi.controller
+class LauteringAutomation(KettleController):
+
+    minSensorDistance = Property.Number("Minimum Distance to Sensor (High Fill Level)", True, 0)
+    maxSensorDistance = Property.Number("Maximum Distance to Sensor (Low Fill Level)", True, 0)
+    pumping = False
+    currentFillLevel = 0
+
+    def stop(self):
+        super(KettleController, self).stop()
+        self.heater_off()
+
+    def run(self):
+        self.sleep(1)
+        while self.is_running():
+            self.currentFillLevel = float(self.get_sensor_value(1))
+            if bool(self.pumping):
+                if self.currentFillLevel >= float(self.maxSensorDistance):
+                    self.heater_off()
+                    self.pumping = False
+                else:
+                    self.heater_on(100)
+            else:
+                if self.currentFillLevel <= float(self.minSensorDistance):
+                    self.heater_on(100)
+                    self.pumping = True
+                else:
+                    self.heater_off()
+            self.sleep(1)
+
